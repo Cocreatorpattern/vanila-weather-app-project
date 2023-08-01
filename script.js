c5dc1579c035a6ce7d091bdc472e84004a5f0324
Cocreatorpattern <136176709+Cocreatorpattern@users.noreply.github.com>
2023-08-01 22:08:01 +0100

    added axios and api keys

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
+function displayTemperature(response) {
+  console.log(response.data.main.temp);
+}
 
+let apiKey = "c2664ee5d87203ac5037da5b7330b87c";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=New York&appid=${apiKey}&units=metric`;
+
+axios.get(apiUrl).then(displayTemperature);
